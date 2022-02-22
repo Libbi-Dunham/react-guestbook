6b63dedba26feb6b388cb266ca4009b1ed709af9
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-02-21 16:53:17 -0800

    wrote out auth for email/password input

--- src/Views/Auth/Auth.js
+++ src/Views/Auth/Auth.js
@@ -1,5 +1,39 @@
 import React from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import { useUser } from '../../Context/UserContext';
+import { useState } from 'react';
 
 export default function Auth() {
-  return <div>Auth</div>;
+  const { setUser } = useUser();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const location = useLocation();
+  const history = useHistory();
+
+  const handleLogin = () => {
+    setUser('example');
+    const { from } = location.state || { from: { pathname: '/' } };
+    history.push(from.pathname);
+    // if (email === process.env.REACT_APP_AUTH_USERNAME) setUser {{ email }}
+  };
+  return (
+    <form>
+      <h2>Auth</h2>
+      <input
+        type="email"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+        placeholder="email"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+        placeholder="password"
+      />
+      <button type="button" onClick={handleLogin}>
+        Enter
+      </button>
+    </form>
+  );
 }
