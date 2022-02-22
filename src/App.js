c0a06793183d0c5328a64d43239624dbd8ac06a0
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-02-21 16:26:07 -0800

    added a login route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from './Views/Footer/Footer';
 import './App.css';
 import EntryList from './Components/EntryList/EntryList';
 import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
+import Auth from './Views/Auth/Auth';
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
       </div>
       <BrowserRouter>
         <Switch>
+          <Route path="/login">
+            <Auth />
+          </Route>
           <PrivateRoute exact path="/">
             <Home />
           </PrivateRoute>
