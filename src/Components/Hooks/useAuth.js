4289f73915810f22090de275fb631d157f73ca20
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-02-22 12:45:30 -0800

    got if not a user an error message pops up

---
+++ src/Components/Hooks/useAuth.js
@@ -0,0 +1,11 @@
+// import { useContext } from 'react';
+// import { authContext } from '../../Context/AuthContext';
+
+// export function useAuth() {
+//   const context = useContext(authContext);
+
+//   if (context === undefined) {
+//     throw new Error('useAuth has to used with ProvideAuth');
+//   }
+//   return context;
+// }
--- src/Views/Auth/Auth.js
+++ src/Views/Auth/Auth.js
@@ -2,43 +2,57 @@ import React from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import { useUser } from '../../Context/UserContext';
 import { useState } from 'react';
+// import { useAuth } from '../../Components/Hooks/useAuth';
 
 export default function Auth() {
   // const { user } = useUser();
   const { setUser } = useUser();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
   const location = useLocation();
   const history = useHistory();
+  // const auth = useAuth();
 
+  const { from } = location.state || { from: { pathname: '/' } };
   const handleLogin = () => {
-    if (email === process.env.REACT_APP_AUTH_USERNAME) setUser({ email });
-    if (password === process.env.REACT_APP_AUTH_USERNAME) setPassword({ password });
+    if (
+      email === process.env.REACT_APP_AUTH_USERNAME &&
+      password === process.env.REACT_APP_AUTH_PASSWORD
+    ) {
+      setUser({ email: email, password: password });
+      setEmail('');
+      setPassword('');
+      history.push(from.pathname);
+    } else {
+      setError('No Success!');
+    }
 
     setUser(email);
-    const { from } = location.state || { from: { pathname: '/' } };
-    history.push(from.pathname);
   };
   return (
-    <form>
-      <h1>Guest Book</h1>
-      <input
-        type="email"
-        value={email}
-        onChange={(event) => setEmail(event.target.value)}
-        placeholder="email"
-      />
-      <div>
+    <>
+      <form>
+        <h1>Guest Book</h1>
         <input
-          type="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-          placeholder="password"
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          placeholder="email"
         />
-      </div>
-      <button className="button" type="button" onClick={handleLogin}>
-        Enter
-      </button>
-    </form>
+        <div>
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            placeholder="password"
+          />
+        </div>
+        <button className="button" type="button" onClick={handleLogin}>
+          Enter
+        </button>
+      </form>
+      {error && <h4>{error}</h4>}
+    </>
   );
 }
